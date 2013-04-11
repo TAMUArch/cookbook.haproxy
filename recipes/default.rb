ad917fae3179fc0e14b581a9255b50574be1dd37
Jim Rosser <jarosser06@gmail.com>
2013-04-11 11:54:54 -0500

    added haproxy install, and configs

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,28 @@
 # Cookbook Name:: haproxy
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, TAMU 
 #
-# All rights reserved - Do Not Redistribute
 #
+package node[:haproxy][:package] do
+  action :install
+end
+
+file "/etc/default/haproxy" do
+  mode "0644"
+  owner "root"
+  group "root"
+  content "ENABLED=1"
+end
+
+template "#{node[:haproxy][:dir]}/#{node[:haproxy][:config]}" do
+  source "haproxy.cfg.erb"
+  mode "0644"
+  owner "root"
+  group "root"
+  notifies :restart, "service[#{node[:haproxy][:service]}]"
+end
+
+service node[:haproxy][:service] do
+  action [:enable, :start]
+end
