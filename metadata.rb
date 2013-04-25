e56e6556b1f7ff1ac83c184b106d07ec71c610d5
Jim Rosser <jarosser06@gmail.com>
2013-04-25 14:16:41 -0500

    added to supported

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'haproxy'
-maintainer       'Texas A&M'
+maintainer       'Jim Rosser'
 maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-supports 'ubuntu', '>= 12.04'
+supports         'ubuntu', '>= 10.04'
+supports         'centos', '>= 6.3'
