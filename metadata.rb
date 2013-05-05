e4f4e53be5ec57880710ea44c6d105638cc8c3f1
Jim <jarosser06@gmail.com>
2013-05-04 20:15:35 -0400

    added centos 5.8 to test-kitchen config

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,4 @@ description      'Installs/Configures haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'ubuntu', '>= 10.04'
-supports         'centos', '>= 6.3'
+supports         'centos', '>= 5.8'
