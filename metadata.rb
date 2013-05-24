c48685a014e1d8247bcfa9ca1a73635c0c82b71a
Jim Rosser <jarosser06@gmail.com>
2013-05-24 09:43:10 -0500

    upped version for development

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jarosser06@arch.tamu.edu'
 license          'MIT'
 description      'Installs/Configures haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 supports         'ubuntu', '>= 10.04'
 supports         'centos', '>= 5.8'
